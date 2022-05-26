9974f5765ac17812258b261256faf5f46622e53a
German <germanmarpar21@gmail.com>
2022-05-25 22:21:19 -0300

    edited the app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,5 @@ import { DatabaseModule } from './common/modules/database/database.module';
 
 @Module({
   imports: [GraphQLModule, DatabaseModule],
-  controllers: [],
-  providers: [],
 })
 export class AppModule {}
