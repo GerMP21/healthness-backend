d811c1f2a0a965727021b2c50e79a03a851f07c3
German Martinez <german.martinez@buscojobs.com>
2022-06-17 18:55:12 -0300

    migrated graphql to the latest version
    Signed-off-by: German Martinez <german.martinez@buscojobs.com>

--- src/common/modules/graphql/graphql.module.ts
+++ src/common/modules/graphql/graphql.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule as GraphQL } from '@nestjs/graphql';
-
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
   imports: [
-    GraphQL.forRoot({
+    GraphQL.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
       autoSchemaFile: 'schema.gql',
       buildSchemaOptions: {
         numberScalarMode: 'integer',
