b5a9b53733a2ed653be7acb3e887965c7322a249
German Martinez <german.martinez@buscojobs.com>
2022-06-24 17:57:15 -0300

    added envirnoment variables for the port and the database string
    Signed-off-by: German Martinez <german.martinez@buscojobs.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,15 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from './common/modules/graphql/graphql.module';
 import { DatabaseModule } from './common/modules/database/database.module';
 import { ExercisesModule } from './modules/exercises/exercises.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [GraphQLModule, DatabaseModule, ExercisesModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    GraphQLModule,
+    DatabaseModule, 
+    ExercisesModule],
 })
 export class AppModule { }
--- src/common/modules/database/database.module.ts
+++ src/common/modules/database/database.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
     imports: [
-        MongooseModule.forRoot('mongodb+srv://app:UdykBLKAihKWL2aA@serverlessinstance0.20oqd.mongodb.net/healthness?retryWrites=true&w=majority', {
+        ConfigModule.forRoot(),
+        MongooseModule.forRoot(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_INSTANCE}.1vh8u.mongodb.net/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority`, 
+        {
             useNewUrlParser: true
         }),
     ],
